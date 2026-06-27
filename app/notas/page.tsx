12473b9bbab87e82b3564994e608147e2f8c2cba
Roberto Luis <Robertoluis@imagemais.com.br>
2026-06-27 00:22:09 -0300

    estilo: adicionar lista de notas e ajustar layout para melhor apresentação

--- app/notas/page.tsx
+++ app/notas/page.tsx
@@ -1,13 +1,18 @@
 import AuroraBackground from "@/components/layout/AuroraBackground";
 import Header from "@/components/layout/Header";
 import Sidebar from "@/components/Notes/Cards/Sidebar";
+import NotesList from "@/components/Notes/Cards/NotesList";
+import { getNotes } from "@/lib/actions/notes";
 
-export default function NotasPage() {
+export default async function NotasPage() {
+  const notes = await getNotes();
+
   return (
     <AuroraBackground>
       <Header />
-      <div className="p-6">
+      <div className="flex gap-6 p-6">
         <Sidebar />
+        <NotesList notes={notes} />
       </div>
     </AuroraBackground>
   );
